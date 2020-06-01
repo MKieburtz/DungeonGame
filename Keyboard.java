e08455532e84de433e65a4a7329f9cbbf1e12d8a
MKieburtz <michaelkieburtz@gmail.com>
2020-05-31 21:52:38 -0700

    first commit

---
+++ Keyboard.java
@@ -0,0 +1,14 @@
+
+//********************************************************************
+//  Keyboard.java       Author: Lewis and Loftus
+//
+//  Facilitates keyboard input by abstracting details about input
+//  parsing, conversions, and exception handling.
+//********************************************************************
+
+import java.util.Scanner;
+
+public class Keyboard
+{
+static Scanner kb = new Scanner(System.in);
+}
---
+++ Warrior.java
@@ -0,0 +1,34 @@
+
+
+/**
+ * Title:
+ * Description:
+ * Copyright:    Copyright (c) 2001
+ * Company:
+ * @author
+ * @version 1.0
+ */
+
+
+
+
+public class Warrior extends Hero
+{
+
+    public Warrior()
+	{
+
+		super("Warrior", 125, 4, .8, 35, 60, .2);
+		specialMove = new CrushingBlow(.4);
+
+    }//end constructor
+
+
+	public void normalAttack(DungeonCharacter opponent)
+	{
+		System.out.println(stats.name + " swings a mighty sword at " +
+							opponent.getName() + ":");
+		super.normalAttack(opponent);
+	}//end override of attack method
+
+}//end Hero class
