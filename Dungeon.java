637a1919371d73c52427c2b8a5cf874f54bdfe17
dnlop95 <43452654+dnlop95@users.noreply.github.com>
2020-06-07 17:57:47 -0700

    Add files via upload
    added movingHero(), isgoNorth(), isgoSouth(), isgoEast(), and isgoWest() method to Dungeon class.
    added movingChoice() method to Hero class.

--- Dungeon.java
+++ Dungeon.java
@@ -1,14 +1,20 @@
 public class Dungeon {
+    private Hero player;
     private static boolean goNorth = false;
     private static boolean goSouth = false;
     private static boolean goEast = false;
     private static boolean goWest = false;
 
-    public static Room[][] newDungeon() {
+    public Dungeon(Hero hero) {
+        this.player = hero;
+        generateDungeon();
+    }
+
+    public static Room[][] generateDungeon() {
         Room[][] dungeon = new Room[5][5];
         for (int i = 0; i < dungeon.length; i++) {
             for(int k = 0; k < dungeon.length; k++) {
-                dungeon[i][k] =
+                //dungeon[i][k] =
             }
         }
         Hero.setCurrRoom(dungeon[1][1]);  //This is where the hero will start
@@ -25,5 +31,30 @@ public class Dungeon {
 
     public boolean colExists(int y) {
         return (y >= 0) && (y <= 4);
+    }
+
+    public void movingHero(Hero hero) {
+        boolean goNorth = roomExists(Hero.getCurrX(), Hero.getCurrY() + 1);
+        boolean goSouth = roomExists(Hero.getCurrX(), Hero.getCurrY() - 1);
+        boolean goEast = roomExists(Hero.getCurrX() + 1, Hero.getCurrY());
+        boolean goWest = roomExists(Hero.getCurrX() - 1, Hero.getCurrY());
+
+    }
+
+
+    public static boolean isgoNorth() {
+        return goNorth;
+    }
+
+    public static boolean isGoSouth() {
+        return goSouth;
+    }
+
+    public static boolean isGoEast() {
+        return goEast;
+    }
+
+    public static boolean isGoWest() {
+        return goWest;
     }
 }
--- Hero.java
+++ Hero.java
@@ -28,9 +28,12 @@
 
 public abstract class Hero extends DungeonCharacter
 {
-	protected double chanceToBlock;  //field
+    protected double chanceToBlock;  //field
 	protected int numTurns;           //field
     protected SpecialMove specialMove;
+    private static int currY;
+    private static int currX;
+    private Room currRoom;
 
 //-----------------------------------------------------------------
 //calls base constructor and gets name of hero from user
@@ -89,9 +92,9 @@ Returns: nothing
 This method calls: defend() or base version of method
 This method is called by: attack() from base class
 ---------------------------------------------------------*/
-public void loseHealth(int hitPoints)
+public void loseHealth(int hitPoints, boolean defendable)
 	{
-		if (defend())
+		if (defendable && defend())
 		{
 			System.out.println(stats.name + " BLOCKED the attack!");
 		}
@@ -147,11 +150,85 @@ This method is called by: external sources
         } while(numTurns > 0);
     }//end battleChoices
 
-    public void info() {
-		System.out.println("Name: " + stats.name);
-		System.out.println("Health: " + stats.hitPoints);
-		System.out.println("Healing Potions:");
-		System.out.println("Vision Potions:");
-		System.out.println("Total pillars found:");
-	}
+    public  void info() {
+	    System.out.println("Name: " + stats.name );
+        System.out.println("In Room ["  + Hero.getCurrX() + "]["+ Hero.getCurrY() + "]");
+	    System.out.println("Health: " + stats.hitPoints );
+	    System.out.println("Healing Potions:" );
+	    System.out.println("Vision Potions:" );
+	    System.out.println("Total pillars found:" );
+    }
+
+    public static void setCurrRoom(Room room) {
+
+    }
+
+    public static int getCurrX() {
+        return currX;
+    }
+
+    public static int getCurrY() {
+        return currY;
+    }
+
+
+    public Room getCurrRoom() {
+        return currRoom;
+    }
+
+
+    public void setCurrX(int currX) {
+        this.currX = currX;
+    }
+
+    public void setCurrY(int currY) {
+        this.currY = currY;
+    }
+
+
+    /*******************************************
+     *  Moving the Hero
+     * This method is deciding which way the hero
+     * should move.
+     */
+
+    public void movingChoice(Hero hero) {
+
+        info();
+        if (Dungeon.isgoNorth()) {
+            System.out.println("Go North (n)\n");
+        }
+        if (Dungeon.isGoSouth()) {
+            System.out.println("Go South (s)\n");
+        }
+        if (Dungeon.isGoEast()) {
+            System.out.println("Go East (e)\n");
+        }
+        if (Dungeon.isGoWest()) {
+            System.out.println("Go West (w)\n");
+        }
+
+        System.out.println("Which way would you like to travel " +
+                "North (n) -- South (s) -- East (e) -- West (w) \n" +
+                "* n * \n" +
+                "w   e \n" +
+                "* s * \n");
+
+        String choice = Keyboard.kb.nextLine();
+        if (choice.equals("n") && Dungeon.isgoNorth()) {
+            hero.setCurrY(getCurrY() + 1);
+        }
+        else if (choice.equals("s") && Dungeon.isGoSouth()) {
+            hero.setCurrY(getCurrY() - 1);
+        }
+        else if (choice.equals("e") && Dungeon.isGoEast()) {
+            hero.setCurrX(getCurrX() + 1);
+        }
+        else if (choice.equals("w") && Dungeon.isGoWest()) {
+            hero.setCurrX(getCurrX() - 1);
+        }
+
+    }//end switch
+
+
 }//end Hero class
